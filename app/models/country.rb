b7aeafbf45806b74c54ee5f4c2c8c5e2b9d0952b
jojogis <jojogis.lesha@yandex.ru>
2021-06-06 14:02:36 +0300

    Добавлена связь страны с тур базами

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,5 +1,6 @@
 class Country < ApplicationRecord
   has_many :regions
+  has_many :tour_base, through: :regions
   validates :name, presence: true, length: { minimum: 2 }
   accepts_nested_attributes_for :regions
 end
