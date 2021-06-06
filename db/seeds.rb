0146fd627f88099f7606c371052c3ffdbdc0cd0e
jojogis <jojogis.lesha@yandex.ru>
2021-06-07 01:29:22 +0300

    Добавлена генерация тестовых данных

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+3.times do
+  country = Country.create(name: Faker::Address.country)
+  3.times do
+    region = Region.create(name: Faker::Address.state, country: country)
+    3.times do
+      city = City.create(name: Faker::Address.city, region: region)
+      3.times do
+        r = Faker::Restaurant
+        TourBase.create(name: r.name, description: r.description,city: city, region: region)
+      end
+    end
+
+    r = Faker::Restaurant
+    TourBase.create(name: r.name, description: r.description,city: nil, region: region)
+
+  end
+end
