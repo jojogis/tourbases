212cdebb62fba4360f11b120a79c32e3833db564
jojogis <jojogis.lesha@yandex.ru>
2021-06-07 01:28:32 +0300

    Добавлены пути для обычного просмотра тур баз

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,1 +1,6 @@
+class MainController < ApplicationController
+  def index
+    redirect_to view_tour_bases_path
+  end
+end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,37 @@
 Rails.application.routes.draw do
   root "main#index"
+  scope :admin do
+    resources :countries do
+      resources :regions
+      resources :tour_bases
+    end
+    resources :regions do
+      resources :cities
+      resources :tour_bases
+    end
 
-  resources :countries do
-    resources :regions
+    resources :cities do
+      resources :tour_bases
+    end
+
     resources :tour_bases
   end
 
-  resources :regions do
-    resources :cities
-    resources :tour_bases
+  namespace :view do
+    resources :tour_bases, only: [:index]
+    resources :countries,only: [] do
+      resources :tour_bases, only: [:index]
+      resources :regions,only: [] do
+        resources :tour_bases, only: [:index]
+        resources :cities,only: [] do
+          resources :tour_bases, only: [:index]
+        end
+      end
+    end
   end
 
-  resources :cities do
-    resources :tour_bases
-  end
 
-  resources :tour_bases
+
 
 
 end
